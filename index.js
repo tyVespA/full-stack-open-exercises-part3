907293fcdc283f517637766ec0a479804ca90e80
tyVespA <migliavacca.marco08@gmail.com>
2025-01-29 12:45:19 +0100

    finish 3.2

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+app.get("/info", (req, res) => {
+  const date = new Date();
+  res.send(
+    `
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${date}</p>
+    `
+  );
+});
+
 const PORT = 3002;
 app.listen(PORT, () => {
   console.log("server running");
