433f38e4f919805bb02f72db3c4d556a3062bc56
tyVespA <migliavacca.marco08@gmail.com>
2025-01-30 19:18:34 +0100

    finish 3.22

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import globals from "globals";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: globals.browser } },
+  {
+    ignores: ["dist/**"],
+  },
+];
